6e7e12ab07258045ba479eec8b113be656a57bfc
stkirk <shaanekirk@gmail.com>
2021-08-24 16:00:56 -0400

    scaffolded files and components

--- random-hyrule-creature/src/App.js
+++ random-hyrule-creature/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import CreatureCard from "./components/CreatureCard";
 
 function App() {
   return (
     <div className="App">
-      <h1>Random Quote</h1>
+      <h1>Legend of Zelda: Breath of the Wild</h1>
+      <button>Random Creature</button>
+      <CreatureCard />
     </div>
   );
 }
---
+++ random-hyrule-creature/src/components/CreatureCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatureCard = (props) => {
+  return (
+    <div className="creature-div">
+      <h3>Creature Info</h3>
+    </div>
+  );
+};
+
+export default CreatureCard;
